52b7d3725f60313eb96c8c180ab70b85ec309f8d
Sjoerd <svanhouten@gmail.com>
2020-12-09 08:23:34 +0100

    day 9 pt1

---
+++ src/main/kotlin/com/github/shmvanhouten/adventofcode2020/day09/XmasDecoder.kt
@@ -0,0 +1,17 @@
+package com.github.shmvanhouten.adventofcode2020.day09
+
+
+fun findFirstNumberThatDoesNotAddUp(input: List<Long>, preambleSize: Int = 25): Long {
+    return input[
+            generateSequence(preambleSize + 1) { it + 1 }
+                .first { doesNotAddUp(input[it], input.subList(it - preambleSize, it)) }
+    ]
+}
+
+fun doesNotAddUp(number: Long, subList: List<Long>): Boolean {
+    return subList.none { addsUp(it, subList - it, number) }
+}
+
+fun addsUp(firstNumber: Long, others: List<Long>, target: Long): Boolean {
+    return others.contains(target - firstNumber)
+}
